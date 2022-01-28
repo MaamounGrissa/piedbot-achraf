8e35c129183fd419b2c73558f1b01c5920659aa1
Achtrick <achrefmtir7@gmail.com>
2022-01-28 14:53:34 +0100

    prod SEO

--- components/Meta.js
+++ components/Meta.js
@@ -4,20 +4,21 @@ export default function Meta() {
   return (
     <Head>
       <link rel="apple-touch-icon" sizes="180x180" href="/favicon.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/16.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/32.png" />
-      <link rel="icon" type="image/png" sizes="48x48" href="/48.png" />
-      <link rel="icon" type="image/png" sizes="96x96" href="/96.png" />
+      <link rel="icon" type="image/png" href="/16.png" />
+      <link rel="icon" type="image/png" href="/32.png" />
+      <link rel="icon" type="image/png" href="/48.png" />
+      <link rel="icon" type="image/png" href="/96.png" />
       <link rel="manifest" href="/favicon/site.webmanifest" />
       <link rel="mask-icon" href="/favicon.png" />
       <link rel="shortcut icon" href="/favicon.png" />
+      <meta charset="utf-8"></meta>
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
       <meta name="theme-color" content="#000" />
       <meta name="robots" content="index, follow" />
       <meta
         name="keywords"
-        content="pied bot, pied, bot, mili, méthode, methode, piedbot, piedbot methode mili, mili method, fraj mili, MILI, methode dynamique, innovation, kinéthérapy, physiothérapy, nouveau né, bébé, clubfoot, foot, club, mili, method, method, clubfoot, clubfoot mili method, mili method, fraj mili, MILI, dynamic method, innovation, kinetherapy, physiotherapy, newborn, baby, حنف القدم ، القدم ، ميلي ، طريقة ، حنف القدم ، طريقة ميلي القدم الحنفاء، القدم الحنفاء ، طريقة ميلي ، فرج ميلي ، ميلي ، الأسلوب الديناميكي ، الابتكار ، العلاج الطبيعي ، حديثي الولادة ، طفل"
+        content="méthode mili pour les pied bot, mili method for clubfoot, pied bot, pied, bot, mili, méthode, methode, piedbot, piedbot methode mili, mili method, fraj mili, MILI, methode dynamique, innovation, kinéthérapy, physiothérapy, nouveau né, bébé, clubfoot, foot, club, mili, method, method, clubfoot, clubfoot mili method, mili method, fraj mili, MILI, dynamic method, innovation, kinetherapy, physiotherapy, newborn, baby, حنف القدم ، القدم ، ميلي ، طريقة ، حنف القدم ، طريقة ميلي القدم الحنفاء، القدم الحنفاء ، طريقة ميلي ، فرج ميلي ، ميلي ، الأسلوب الديناميكي ، الابتكار ، العلاج الطبيعي ، حديثي الولادة ، طفل"
       />
       <meta property="og:image" content="/favicon.png" />
       <link
